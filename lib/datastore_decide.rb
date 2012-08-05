38d0ba6a0d7fa4e62a86b860448e5d1c6949fa3b
Stephen Tiberius Schor <beholdthepanda@gmail.com>
2012-08-05 12:02:19 -0300

    Update lib/datastore_decide.rb
    Adding more datastores.

--- lib/datastore_decide.rb
+++ lib/datastore_decide.rb
@@ -1,7 +1,7 @@
 module DatastoreDecide
 
   def self.decide
-    dbs = ["Mongo", "Cassandra", "Redis", "MySQL", "a big hash", "Hadoop", "Neo4j", "something you make yourself", "your face"]
+    dbs = ["Mongo", "Cassandra", "Redis", "MySQL", "Kyoto Cabinet", "HBase", "a big hash", "Hadoop", "Neo4j", "something you make yourself", "your face"]
     puts "Just store it in #{dbs.shuffle[0]}."
   end
 
